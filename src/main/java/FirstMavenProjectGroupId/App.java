8975b6ada0f1f85fe390113a2a95665d96c55deb
olbe0615 <oleg.berest@NetCracker.com>
2018-10-16 10:46:50 +0300

    Initial commit

---
+++ src/main/java/FirstMavenProjectGroupId/App.java
@@ -0,0 +1,13 @@
+package FirstMavenProjectGroupId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
